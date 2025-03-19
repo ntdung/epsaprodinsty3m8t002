a272ff95268f9d123b2c35b3f10f2fd7db61580a
Dung Nguyen <dung.nguyen@optimizely.com>
2025-03-19 23:33:11 +0700

    Reset to the core files

---
+++ apps/frontend/src/components/factory.ts
@@ -0,0 +1,23 @@
+import 'server-only'
+import { type ComponentFactory, DefaultComponentFactory, RichTextComponentDictionary } from '@remkoj/optimizely-cms-react/rsc'
+import { cache } from 'react'
+import cmsComponents from './cms'
+
+/**
+ * Get the cached version of the Component Factory to use, this ensure that the
+ * minimum number of instances of the factory will be created.
+ */
+export const getFactory = cache<() => ComponentFactory>(() => 
+{
+    const factory = new DefaultComponentFactory()
+
+    // Add support for Rich Text
+    factory.registerAll(RichTextComponentDictionary)
+
+    // Add support for Frontend defined components
+    factory.registerAll(cmsComponents)
+
+    return factory
+})
+
+export default getFactory
